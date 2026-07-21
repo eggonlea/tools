b727d19ce0356c7a1995c1d772e34d28a2c9faa7
Li Li <eggonlea@msn.com>
2026-07-21 02:31:42 -0700

    add apt install for cuttlefish host tools
    Signed-off-by: Li Li <eggonlea@msn.com>

--- bin/ssh_codespaces_blue.sh
+++ bin/ssh_codespaces_blue.sh
@@ -2,6 +2,11 @@
 
 set -x
 
+# WiFi
 # echo "sudo ip route add 139.9.131.65 via 100.69.96.1 dev wlp0s20f3 table main"
+# echo "sudo ip route add 139.9.131.65 via 100.69.38.1 dev wlp0s20f3 table main"
+
+# USB Ethernet
+# echo "sudo ip route add 139.9.131.65 via 192.168.22.1 dev enx94bdbe11210e table main"
 
 ssh -R 8080:localhost:8080 -R 8081:localhost:8081 -p 41422 -i ~/.ssh/id_ed25519 huawei@139.9.131.65
